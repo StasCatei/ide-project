09ecc7de5dedc91bcd311c88e51de8e7882413a1
StasCatei <stanislav.catei@elev.cihcahul.md>
2022-01-31 10:59:49 +0200

    Make header responsive

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,20 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 /* Document ready put your JavaScript code here */
-$(() => {
-  
-});
+$(() => {});
+
+const mainMenu = document.querySelector(".menu-mobile");
+const openMenu = document.querySelector(".burger");
+const closeMenu = document.querySelector(".x");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
+
+function close() {
+  mainMenu.style.display = 'none';
+}

d5b6c4ccbdb376635d44d1165dbea3df9645b8a6
StasCatei <stanislav.catei@elev.cihcahul.md>
2022-01-31 16:10:50 +0200

    Make header full resonsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
+/*Import aditional javascript files*/
+import './header';
+
 /* Import Styles */
 import '../scss/app.scss';
 
@@ -7,19 +10,3 @@ if (process.env.NODE_ENV !== 'production') {
 
 /* Document ready put your JavaScript code here */
 $(() => {});
-
-const mainMenu = document.querySelector(".menu-mobile");
-const openMenu = document.querySelector(".burger");
-const closeMenu = document.querySelector(".x");
-
-openMenu.addEventListener("click", show);
-closeMenu.addEventListener("click", close);
-
-function show() {
-  mainMenu.style.display = 'flex';
-  mainMenu.style.top = '0';
-}
-
-function close() {
-  mainMenu.style.display = 'none';
-}
